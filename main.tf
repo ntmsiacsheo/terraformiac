0a9a61aaaf5c391bf5e2f5845e9fa192a5a22df8
Sheo Vikesh <vikeshsheo@outlook.com>
2022-01-15 10:42:35 +0530

    commmit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "Sheo-Vikesh-RG"
+  location = "West Europe"
+}
